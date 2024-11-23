b273161c2ad296fc591ce91959978db5b751ab09
Mikhail <mikthefirst11@gmail.com>
2024-11-23 14:15:56 +0300

    Added registration

--- src/App.js
+++ src/App.js
@@ -1,104 +1,19 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Table } from 'antd';
-import AddVacancy from './components/Add/AddVacancy'
-
-
-const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-  {
-    key: '3',
-    name: 'Lucy',
-    age: 30,
-    address: '221B Baker Street',
-  },
-  {
-    key: '4',
-    name: 'Jack',
-    age: 29,
-    address: '742 Evergreen Terrace',
-  },
-  {
-    key: '5',
-    name: 'Rose',
-    age: 35,
-    address: '12 Grimmauld Place',
-  },
-
-  {
-    key: '7',
-    name: 'Rose',
-    age: 35,
-    address: '12 Grimmauld Place',
-  },
-
-];
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
-
-
+import VacancyTable from './components/Table/Table';
+import Home from './components/UserReg/Home';
 
 function App() {
-
-  const [AddVacancyVisible, setAddVacancyVisible] = useState(false);
-
-  const changeAddVacancy = () => {
-    setAddVacancyVisible(!setAddVacancyVisible);
-  }
-
-
-  const OnRowClick = (record) => {
-    return {
-      onClick: () => {
-        console.log('Row clicked:', record);
-        setAddVacancyVisible(!AddVacancyVisible);
-      }
-    }
-  }
   return (
-    <div className="App">
-
-      {AddVacancyVisible &&
-        (<AddVacancy onClose={changeAddVacancy} />)
-      }
-
-      <h1 className="ant-table-title">Vacancies List</h1>
-      <Table
-        dataSource={dataSource}
-        columns={columns}
-        pagination={{ position: ['bottomCenter'] }}
-        onRow={OnRowClick}
-      />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vacancies" element={<VacancyTable />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/UserReg/Home.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import styles from './home.module.css';
+
+console.log('env string:', `${process.env.ConnectSTR}/AddUser`);
+
+function Home() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [file, setFile] = useState(null);
+
+    const joinRoom = async () => {
+        if (username !== '' && password !== '' && file !== null) {
+            console.log('working:', { username, password, file });
+
+            const formData = new FormData();
+            formData.append('username', username);
+            formData.append('password', password);
+            formData.append('file', file);
+            //http://localhost:8080/app/AddUser/
+            try {
+                const response = await fetch(`http://localhost:8080/app/AddUser/`, {
+                    method: 'POST',
+                    body: formData,
+                    credentials: 'include'
+                });
+                console.log('responce status:', response.status);
+                if (document.cookie.username) {
+                    document.location.href = '/vacancies';
+                } else {
+                    console.error('Registration failed');
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+                <h1 className={styles.WhiteSpace}>{`Registration`}<br /></h1>
+                <input
+                    className={styles.input}
+                    placeholder='Username...'
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    className={styles.input}
+                    placeholder='Password...'
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="file"
+                    className={styles.input}
+                    placeholder='Resume'
+                    onChange={(e) => setFile(e.target.files[0])}
+                />
+                <button
+                    className={`${styles.btn} btn btn-secondary`}
+                    style={{ width: '100%' }}
+                    onClick={joinRoom}>
+                    Sign in
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
